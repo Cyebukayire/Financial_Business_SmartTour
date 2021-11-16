43daf0a0ff2256ed4d74c1889ca20b39d87ab4d1
Odomero <oshieodomero@gmail.com>
2021-11-16 22:35:30 +0100

    Updated Django_Secret

--- SmarTour/SmarTour/settings.py
+++ SmarTour/SmarTour/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%r3pvo5d47-grvoo385bllyc5hs&jy56b*_k4z*gh-o-o2#*5a'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
